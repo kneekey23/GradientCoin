5c9a09b7a7591018292137a94c516be62f8e5641
kneekey23 <nicoleklein@Nicoles-MacBook-Pro.local>
2018-05-31 21:37:29 -0700

    tests added

--- test/gradientcoin.js
+++ test/gradientcoin.js
@@ -1,64 +1,146 @@
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const GradientCoin = artifacts.require("./GradientCoin.sol");
 
-contract('MetaCoin', function(accounts) {
-  it("should put 10000 MetaCoin in the first account", function() {
-    return MetaCoin.deployed().then(function(instance) {
-      return instance.getBalance.call(accounts[0]);
-    }).then(function(balance) {
-      assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
-    });
-  });
-  it("should call a function that depends on a linked library", function() {
-    var meta;
-    var metaCoinBalance;
-    var metaCoinEthBalance;
+// Turning should statements into tests with truffle
+// describe, it, beforeEach, contract
 
-    return MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(accounts[0]);
-    }).then(function(outCoinBalance) {
-      metaCoinBalance = outCoinBalance.toNumber();
-      return meta.getBalanceInEth.call(accounts[0]);
-    }).then(function(outCoinBalanceEth) {
-      metaCoinEthBalance = outCoinBalanceEth.toNumber();
-    }).then(function() {
-      assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, "Library function returned unexpeced function, linkage may be broken");
-    });
-  });
+contract('GradientCoin', function (accounts) {
+  it("should return 'GradientCoin' as its name", function() {
+   return GradientCoin.deployed()
+    .then(function (instance) {
+      return instance.name.call();
+    })
+    .then(function(name) {
+      assert.equal(name, "GradientCoin", "Name did not return correctly")
+    })
+  })
 
-  it("should send coin correctly", function() {
-    var meta;
+  it("should return 'GRD' as its symbol", function() {
+    return GradientCoin.deployed()
+      .then(function(instance) {
+        return instance.symbol.call()
+      })
+      .then(function(symbol) {
+        assert.equal(symbol, "GRD", "Symbol did not return correctly")
+      })
+  })
 
-    //    Get initial balances of first and second account.
-    var account_one = accounts[0];
-    var account_two = accounts[1];
+  it("should have a way to mint new tokens and return the new token's ID", function() {
+    return GradientCoin.deployed()
+      .then(function(instance) {
+        return instance.mint("testOuter", "testInner", {from: accounts[0]});
+      })
+      .then(function(receipt) {
+        let tokenId = receipt.logs[0].args.tokenId.toNumber();
+        assert.equal(tokenId, 0, "The id did not return as expected");
+      })
+  })
 
-    var account_one_starting_balance;
-    var account_two_starting_balance;
-    var account_one_ending_balance;
-    var account_two_ending_balance;
+  it("should broadcast a Mint event whenever a new token is minted", function() {
+    return GradientCoin.deployed()
+      .then(function(instance) {
+        return instance.mint("testOuter2", "testInner2", {from: accounts[0]});
+      })
+      .then(function(receipt) {
+        let event = receipt.logs[0].event;
+        assert.equal(event, "Mint", "The Mint event did not return as expected");
+      })
+  })
 
-    var amount = 10;
+  it("should broadcast a Transfer event whenever a new token is minted", function() {
+    return GradientCoin.deployed()
+      .then(function(instance) {
+        return instance.mint("testOuter3", "testInner3", {from: accounts[0]});
+      })
+      .then(function(receipt) {
+        let event = receipt.logs[1].event;
+        assert.equal(event, "Transfer", "The Transfer event did not return as expected");
+      })
+  })
+  
+  it("should have a way to return the id of all tokens owned by an address", function() {
+    return GradientCoin.deployed()
+      .then(function(instance) {
+        return instance.tokensOfOwner.call(accounts[0]);
+      })
+      .then(function(tokens) {
+        assert.equal(tokens[0].toNumber(), 1, "The Contract did not return the correct number of tokens for this address");
+        assert.equal(tokens[1].toNumber(), 2, "The Contract did not return the correct number of tokens for this address");
+        assert.equal(tokens[2].toNumber(), 0, "The Contract did not return the correct number of tokens for this address");
+      })
+  })
 
-    return MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account_one);
-    }).then(function(balance) {
-      account_one_starting_balance = balance.toNumber();
-      return meta.getBalance.call(account_two);
-    }).then(function(balance) {
-      account_two_starting_balance = balance.toNumber();
-      return meta.sendCoin(account_two, amount, {from: account_one});
-    }).then(function() {
-      return meta.getBalance.call(account_one);
-    }).then(function(balance) {
-      account_one_ending_balance = balance.toNumber();
-      return meta.getBalance.call(account_two);
-    }).then(function(balance) {
-      account_two_ending_balance = balance.toNumber();
+  it("should have a way to retreive the address of the owner of a coin using its token ID", function() {
+    return GradientCoin.deployed()
+      .then(function(instance) {
+        return instance.ownerOf.call(1);
+      })
+      .then(function(address) {
+        assert.equal(address, accounts[0], "The outer property did not return as expected");
+      })
+  })
+  
+  it("should have a way to retreive the properties of a coin using its token ID", function() {
+    return GradientCoin.deployed()
+      .then(function(instance) {
+        return instance.getToken.call(1);
+      })
+      .then(function(token) {
+        assert.equal(token[0], "testOuter2", "The outer property did not return as expected");
+        assert.equal(token[1], "testInner2", "The inner property did not return as expected");
+      })
+  })
 
-      assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
-      assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
-    });
-  });
+  it("should have a way to retreive the total number of tokens an address has", function() {
+    return GradientCoin.deployed()
+      .then(function(instance) {
+        return instance.balanceOf.call(accounts[0]);
+      })
+      .then(function(numTokens) {
+        assert.equal(numTokens, 3, "The Contract did not return the correct number of tokens for this address");
+      })
+  })
+
+  it("should have a way for the sender to approve one of his/her tokens for transfer to another address", function() {
+    return GradientCoin.deployed()
+      .then(function(instance) {
+        return instance.approve(accounts[1], 1, {from: accounts[0]})
+          .then(function () { return instance.tokenIndexToApproved.call(1)});
+      })
+      .then(function(approvedAddress) {
+        assert.equal(approvedAddress, accounts[1], "The approved address was different than expected");
+      })
+  })
+
+  it("should broadcast the Approval event whenever a token owner approves that token for transfer to a new address", function() {
+      return GradientCoin.deployed()
+        .then(function(instance) {
+          return instance.approve(accounts[1], 2, {from: accounts[0]});
+        })
+        .then(function(receipt) {
+          let event = receipt.logs[0].event;
+          assert.equal(event, "Approval", "The Transfer event did not return as expected");
+        })
+  })
+
+  it("should have a way to show that a transferred token is owned by the new address", function() {
+    return GradientCoin.deployed()
+      .then(function(instance) {
+        return instance.transfer(accounts[1], 1, {from: accounts[0]})
+          .then(function () { return instance.ownerOf.call(1)});
+      })
+      .then(function(tokenOwner) {
+        assert.equal(tokenOwner, accounts[1], "The Contract did not transfer the token as expected");
+      })
+  })
+  
+  it("should broadcast the Transfer event whenever a token is transferred to a new address", function() {
+    return GradientCoin.deployed()
+      .then(function(instance) {
+        return instance.transfer(accounts[1], 2, {from: accounts[0]});
+      })
+      .then(function(receipt) {
+        let event = receipt.logs[0].event;
+        assert.equal(event, "Transfer", "The Transfer event did not fire off as expected");
+      })
+  })
 });
